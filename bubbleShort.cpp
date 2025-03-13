084f1ab8c4c47c8d8d0c4d41bc8e84825129510a
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-03-13 18:50:21 +0700

    Add Void For Input data array

--- bubbleShort.cpp
+++ bubbleShort.cpp
@@ -4,3 +4,30 @@ using namespace std;
 int arr[20];
 int n;
 
+void inputData(){
+    while (true)
+    {
+        cout << "Masukkan jumlah data: ";
+        cin >> n;
+
+        if (n < 15){
+            break;
+        }
+        else
+        {
+            cout << "Jumlah data maksimal 15" << endl;
+        }
+    }
+
+
+    cout << "=============" << endl;
+    cout << "Masukkan Data" << endl;
+    cout << "=============" << endl;
+    cout << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Masukkan data ke- " << i + 1 << "=";
+        cin >> arr[i];
+    }
+}
